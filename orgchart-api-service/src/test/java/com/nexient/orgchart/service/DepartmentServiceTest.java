284f53b1123e5e7241553b8f78e6f07b2380d57f
dhoover <dhoover@nexient.com>
2016-08-04 10:44:12 -0400

    added test for findByInactiveDepartment

--- orgchart-api-service/src/test/java/com/nexient/orgchart/service/DepartmentServiceTest.java
+++ orgchart-api-service/src/test/java/com/nexient/orgchart/service/DepartmentServiceTest.java
@@ -24,121 +24,121 @@ import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
-
 public class DepartmentServiceTest {
 
-	@InjectMocks
-	private DepartmentService departmentService;
+    @InjectMocks
+    private DepartmentService departmentService;
 
-	private DepartmentEntity department;
+    @Spy
+    private DepartmentMapper departmentMapper = new DepartmentMapper();
 
-	@Spy
-	private DepartmentMapper departmentMapper = new DepartmentMapper();
+    @Spy
+    private EmployeeMapper employeeMapper = new EmployeeMapper();
 
-	@Spy
-	private EmployeeMapper employeeMapper = new EmployeeMapper();
+    @Mock
+    private DepartmentRepository repo;
 
-	@Mock
-	private DepartmentRepository repo;
+    private DepartmentEntity department;
+    private List<DepartmentEntity> listOfFoundDepts;
 
-	private List<DepartmentEntity> listOfFoundDepts;
+    @BeforeTest
+    public void before() {
+        MockitoAnnotations.initMocks(this);
 
-	@BeforeTest
-	public void before(){
-		MockitoAnnotations.initMocks(this);
+        listOfFoundDepts = new ArrayList<>();
+        listOfFoundDepts.add(Entities.department());
 
-		listOfFoundDepts = new ArrayList<>();
-		listOfFoundDepts.add(Entities.department());
+        department = Entities.department();
+        department.setId(Entities.DEPT_ID);
 
-		department=Entities.department();
-		department.setId(Entities.DEPT_ID);
+        departmentMapper.setEmployeeMapper(employeeMapper);
 
-		departmentMapper.setEmployeeMapper(employeeMapper);
+        when(repo.findAll()).thenReturn(this.listOfFoundDepts);
+        when(repo.findOne(Entities.DEPT_ID)).thenReturn(this.department);
+        when(repo.save(any(DepartmentEntity.class))).thenReturn(this.department);
+        when(repo.findByIsActiveIsTrue()).thenReturn(this.listOfFoundDepts);
+        when(repo.findByManager(any(EmployeeEntity.class))).thenReturn(this.listOfFoundDepts);
+        when(repo.findByParentDepartment(any(DepartmentEntity.class))).thenReturn(this.listOfFoundDepts);
+        when(repo.findByIsActiveIsFalse()).thenReturn(this.listOfFoundDepts);
+    }
 
-		when(repo.findAll()).thenReturn(this.listOfFoundDepts);
-		when(repo.findOne(Entities.DEPT_ID)).thenReturn(this.department);
-		when(repo.save(any(DepartmentEntity.class))).thenReturn(this.department);
-		when(repo.findByIsActiveIsTrue()).thenReturn(this.listOfFoundDepts);
-		when(repo.findByManager(any(EmployeeEntity.class))).thenReturn(this.listOfFoundDepts);
-		when(repo.findByParentDepartment(any(DepartmentEntity.class))).thenReturn(this.listOfFoundDepts);
-		when(repo.findByIsActiveIsFalse()).thenReturn(this.listOfFoundDepts);
-	}
-	@Test
-	public void findAllDepartments() {
-		List<Department> depts = this.departmentService.findAllDepartments();
-		assertNotNull(depts);
-		Assert.assertTrue(depts.size() > 0);
-	}
+    @Test
+    public void findAllDepartments() {
+        List<Department> depts = this.departmentService.findAllDepartments();
+        assertNotNull(depts);
+        Assert.assertTrue(depts.size() > 0);
+    }
 
-	@Test
-	public void findDepartment() {
-		Department dept = this.departmentService.findDepartmentByID(Entities.DEPT_ID);
-		assertNotNull(dept);
-		assertEquals(Entities.DEPT_ID, dept.getId());
-	}
+    @Test
+    public void findDepartment() {
+        Department dept = this.departmentService.findDepartmentByID(Entities.DEPT_ID);
+        assertNotNull(dept);
+        assertEquals(Entities.DEPT_ID, dept.getId());
+    }
 
-	@Test
-	public void storeDepartment() {
-		Department dept = this.departmentService.storeOrUpdate(departmentMapper.entityToModel(this.department));
-		Assert.assertNotNull(dept);
-		Assert.assertEquals(Entities.DEPT_ID, dept.getId(), "Expected " + Entities.DEPT_ID + " but got " + dept.getId());
-	}
+    @Test
+    public void storeDepartment() {
+        Department dept = this.departmentService.storeOrUpdate(departmentMapper.entityToModel(this.department));
+        Assert.assertNotNull(dept);
+        Assert.assertEquals(Entities.DEPT_ID, dept.getId(), "Expected " + Entities.DEPT_ID + " but got " + dept.getId());
+    }
 
-	@Test
-	public void removeDepartment() {
-		this.department.setIsActive(true);
-		this.department.setParentDepartment(Entities.department());
-		this.department.setManager(Entities.manager());
+    @Test
+    public void removeDepartment() {
+        this.department.setIsActive(true);
+        this.department.setParentDepartment(Entities.department());
+        this.department.setManager(Entities.manager());
 
-		Assert.assertTrue(departmentService.removeDepartment(this.department.getId()));
-	}
+        Assert.assertTrue(departmentService.removeDepartment(this.department.getId()));
+    }
 
-	@Test
-	public void removeDepartment_False(){
-		this.department.setIsActive(true);
+    @Test
+    public void removeDepartment_False() {
+        this.department.setIsActive(true);
 
-		doAnswer(new Answer<DepartmentEntity>() {
-			@Override
-			public DepartmentEntity answer(InvocationOnMock invocation) throws Throwable {
-				Object[] args = invocation.getArguments();
-				DepartmentEntity depy = (DepartmentEntity) args[0];
-				depy.setIsActive(true);
-				return depy;
-			}
-		}).when(this.repo).save(any(DepartmentEntity.class));
+        doAnswer(new Answer<DepartmentEntity>() {
+            @Override
+            public DepartmentEntity answer(InvocationOnMock invocation) throws Throwable {
+                Object[] args = invocation.getArguments();
+                DepartmentEntity depy = (DepartmentEntity) args[0];
+                depy.setIsActive(true);
+                return depy;
+            }
+        }).when(this.repo).save(any(DepartmentEntity.class));
 
-		Assert.assertFalse(this.departmentService.removeDepartment(department.getId()));
-	}
-	@Test
-	public void updateDepartment() throws Exception {
-		department.setName("Other Dept Name");
+        Assert.assertFalse(this.departmentService.removeDepartment(department.getId()));
+    }
 
-		departmentService.storeOrUpdate(departmentMapper.entityToModel(this.department));
-		Assert.assertEquals(this.department.getName(), departmentService.findDepartmentByID(department.getId()).getName());
-	}
+    @Test
+    public void updateDepartment() throws Exception {
+        department.setName("Other Dept Name");
 
-	@Test
-	public void findAllActiveDepartments() {
-		List<Department> depts = departmentService.findAllActiveDepartments();
-		Assert.assertNotNull(depts);
-		Assert.assertTrue(depts.size()>0);
-	}
+        departmentService.storeOrUpdate(departmentMapper.entityToModel(this.department));
+        Assert.assertEquals(this.department.getName(), departmentService.findDepartmentByID(department.getId()).getName());
+    }
 
-	@Test
-	public void findAllInactiveDepartments() {
-		List<Department> depts = departmentService.findAllInactiveDepartments();
-		Assert.assertNotNull(depts);
-		Assert.assertTrue(depts.size()>0);
+    @Test
+    public void findAllActiveDepartments() {
+        List<Department> depts = departmentService.findAllActiveDepartments();
+        Assert.assertNotNull(depts);
+        Assert.assertTrue(depts.size() > 0);
+    }
 
-	}
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void storeDepartment_Null()throws Exception{
-        this.departmentService.storeOrUpdate(null);
+    @Test
+    public void findAllInactiveDepartments() {
+        List<Department> depts = departmentService.findAllInactiveDepartments();
+        Assert.assertNotNull(depts);
+        Assert.assertTrue(depts.size() > 0);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
-    public void removeDepartment_Null()throws Exception{
+    public void storeDepartment_Null() throws Exception {
+        this.departmentService.storeOrUpdate(null);
+
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void removeDepartment_Null() throws Exception {
         this.departmentService.removeDepartment(null);
     }
 
