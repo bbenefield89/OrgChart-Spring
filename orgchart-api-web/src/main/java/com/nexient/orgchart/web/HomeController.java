311c362c9c454cc255bd0c55c1ed3bd5078ac89e
dhoover <dhoover@nexient.com>
2016-08-04 15:53:42 -0400

    created HomeControllerTest
    -100% pit, 100% line coverage

--- orgchart-api-web/src/main/java/com/nexient/orgchart/web/HomeController.java
+++ orgchart-api-web/src/main/java/com/nexient/orgchart/web/HomeController.java
@@ -2,13 +2,16 @@ package com.nexient.orgchart.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by mrangel on 7/26/2016.
  */
+@RestController
+@RequestMapping("/home")
 public class HomeController {
 
-    @RequestMapping(value = "home", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String doGet() {
         return View.HOME;
     }
---
+++ orgchart-api-web/src/test/java/com/nexient/orgchart/web/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.nexient.orgchart.web;
+
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Created by dhoover on 8/4/2016.
+ */
+public class HomeControllerTest {
+
+    @InjectMocks
+    HomeController controller;
+
+    @BeforeClass
+    public void before(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testHomeDoGet(){
+        Assert.assertNotNull(controller.doGet());
+    }
+}
