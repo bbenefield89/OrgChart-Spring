41c7f3c4e387b00bf818040eaa09fe5c098144ca
Brandon Benefield <bbenefield@nexient.com>
2019-01-08 17:42:12 -0500

    FLESH OUT 'EmployeeEntity' CLASS

--- orgchart-api-data/src/main/java/com.nexient.orgchart/data/entity/EmployeeEntity.java
+++ orgchart-api-data/src/main/java/com.nexient.orgchart/data/entity/EmployeeEntity.java
@@ -1,5 +1,34 @@
 package com.nexient.orgchart.data.entity;
 
 public class EmployeeEntity extends BaseEntity {
-	//GL HF ;)
+	private String firstName;
+	private char middleInitial;
+	private String lastName;
+	private String email;
+	private String skypeName;
+	private boolean isManager;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setMiddleInitial(char middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSkypeName(String skypeName) {
+        this.skypeName = skypeName;
+    }
+
+    public void setIsManager(boolean manager) {
+        isManager = manager;
+    }
 }
