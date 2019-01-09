383f988d61392b04ffd3590b6e447f32f054dc5f
bbenefield <bbenefield@nexient.com>
2019-01-09 16:16:18 -0500

    FLESH OUT 'JobTitle' CLASS
    - Props:
      - name
    
    - Methods:
      - setName

--- orgchart-api-model/src/main/java/com/nexient/orgchart/model/JobTitle.java
+++ orgchart-api-model/src/main/java/com/nexient/orgchart/model/JobTitle.java
@@ -1,5 +1,9 @@
 package com.nexient.orgchart.model;
 
 public class JobTitle extends BaseModel {
+    private String name;
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
