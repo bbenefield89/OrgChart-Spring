e95da044bd458d6e0bafd984ce7693a338eb5cf8
bbenefield <bbenefield@nexient.com>
2019-01-09 16:46:36 -0500

    ADD NEW METHOD
    - JobTitle.java
      - Methods:
        - getName

--- orgchart-api-model/src/main/java/com/nexient/orgchart/model/JobTitle.java
+++ orgchart-api-model/src/main/java/com/nexient/orgchart/model/JobTitle.java
@@ -3,6 +3,10 @@ package com.nexient.orgchart.model;
 public class JobTitle extends BaseModel {
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
