5282f4cf728b466768d4183ae3e944d70e9e317d
bbenefield <bbenefield@nexient.com>
2019-01-09 16:13:28 -0500

    FLESH OUT 'Employee' MODEL
    - Props:
      - id
      - department
      - firstName
      - middleInitial
      - lastName
      - jobTitle
      - email
      - skypeName
      - isManager
    
    - Methods
      - getId, setId
      - setDepartment
      - setFirstName
      - setMiddleInitial
      - setLastName
      - setJobTitle
      - setEmail
      - setSkypeName
      - setIsManager

--- orgchart-api-model/src/main/java/com/nexient/orgchart/model/Employee.java
+++ orgchart-api-model/src/main/java/com/nexient/orgchart/model/Employee.java
@@ -1,5 +1,53 @@
 package com.nexient.orgchart.model;
 
 public class Employee {
+    private int id;
+    private Department department;
+    private String firstName;
+    private char middleInitial;
+    private String lastName;
+    private JobTitle jobTitle;
+    private String email;
+    private String skypeName;
+    private boolean isManager;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setMiddleInitial(char middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setJobTitle(JobTitle jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSkypeName(String skypeName) {
+        this.skypeName = skypeName;
+    }
+
+    public void setIsManager(boolean manager) {
+        isManager = manager;
+    }
 }
