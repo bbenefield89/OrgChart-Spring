9cb3ad8b11aa47ab5f4d32f522777237a3becbc4
Brandon Benefield <bbenefield@nexient.com>
2019-01-09 10:55:11 -0500

    ADD NEW PROPS/GETTERS/SETTERS REQUIRED TO PASS TESTS
    - EmployeeEntity.java

--- orgchart-api-data/src/main/java/com.nexient.orgchart/data/entity/EmployeeEntity.java
+++ orgchart-api-data/src/main/java/com.nexient.orgchart/data/entity/EmployeeEntity.java
@@ -1,15 +1,34 @@
 package com.nexient.orgchart.data.entity;
 
+import java.util.Set;
+
 public class EmployeeEntity extends BaseEntity {
 	private String firstName;
 	private char middleInitial;
 	private String lastName;
 	private String email;
 	private String skypeName;
+    private JobTitleEntity jobTitle;
+    private EmployeeEntity manager;
+    private DepartmentEntity department;
+    private Set<EmployeeEntity> ManagedEmployees;
+    private Set<DepartmentEntity> ManagedDepartments;
 	private boolean isManager;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public char getMiddleInitial() {
+        return middleInitial;
     }
 
     public void setMiddleInitial(char middleInitial) {
@@ -20,12 +39,64 @@ public class EmployeeEntity extends BaseEntity {
         this.lastName = lastName;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getSkypeName() {
+        return skypeName;
+    }
+
     public void setSkypeName(String skypeName) {
         this.skypeName = skypeName;
+    }
+
+    public JobTitleEntity getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(JobTitleEntity jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public EmployeeEntity getManager() {
+        return manager;
+    }
+
+    public void setManager(EmployeeEntity manager) {
+        this.manager = manager;
+    }
+
+    public DepartmentEntity getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(DepartmentEntity department) {
+        this.department = department;
+    }
+
+    public Set<EmployeeEntity> getManagedEmployees() {
+        return ManagedEmployees;
+    }
+
+    public void setManagedEmployees(Set<EmployeeEntity> managedEmployees) {
+        ManagedEmployees = managedEmployees;
+    }
+
+    public Set<DepartmentEntity> getManagedDepartments() {
+        return ManagedDepartments;
+    }
+
+    public void setManagedDepartments(Set<DepartmentEntity> managedDepartments) {
+        ManagedDepartments = managedDepartments;
+    }
+
+    public boolean getIsManager() {
+        return isManager;
     }
 
     public void setIsManager(boolean manager) {
