13936f40aeafc7f7e674895c85fb0a263f9443da
bbenefield <bbenefield@nexient.com>
2019-01-09 16:41:24 -0500

    CREATE NEW PROPS/METHODS
    - Employee.java
      - Props:
        - manager
    
      - Methods:
        - getDepartment
        - getManager, setManager
        - getFirstName
        - getMiddleInitial
        - getLastName
        - getJobTitle
        - getEmail
        - getSkypeName
        - getIsManager
    
    - EmployeeTests.java
      - tests passing

--- orgchart-api-model/src/main/java/com/nexient/orgchart/model/Employee.java
+++ orgchart-api-model/src/main/java/com/nexient/orgchart/model/Employee.java
@@ -3,6 +3,7 @@ package com.nexient.orgchart.model;
 public class Employee {
     private int id;
     private Department department;
+    private Employee manager;
     private String firstName;
     private char middleInitial;
     private String lastName;
@@ -19,32 +20,72 @@ public class Employee {
         this.id = id;
     }
 
+    public Department getDepartment() {
+        return department;
+    }
+
     public void setDepartment(Department department) {
         this.department = department;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public String getFirstName() {
+        return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    public char getMiddleInitial() {
+        return middleInitial;
+    }
+
     public void setMiddleInitial(char middleInitial) {
         this.middleInitial = middleInitial;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    public JobTitle getJobTitle() {
+        return jobTitle;
+    }
+
     public void setJobTitle(JobTitle jobTitle) {
         this.jobTitle = jobTitle;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getSkypeName() {
+        return skypeName;
+    }
+
     public void setSkypeName(String skypeName) {
         this.skypeName = skypeName;
+    }
+
+    public boolean getIsManager() {
+        return isManager;
     }
 
     public void setIsManager(boolean manager) {
--- orgchart-api-model/src/test/java/com/nexient/orgchart/model/EmployeeTest.java
+++ orgchart-api-model/src/test/java/com/nexient/orgchart/model/EmployeeTest.java
@@ -22,86 +22,86 @@ public class EmployeeTest {
 
     Random random = new Random();
 
-//    @BeforeSuite
-//    public void before() {
-//        emp = new Employee();
-//        mgr = new Employee();
+    @BeforeSuite
+    public void before() {
+        emp = new Employee();
+        mgr = new Employee();
 //        mgr.setId(random.nextInt());
-//        dept = new Department();
+        dept = new Department();
 //        dept.setId(random.nextInt());
-//        title=Models.jobTitle();
-//    }
+        title=Models.jobTitle();
+    }
 
     @Test
     public void instantiated() {
         Assert.assertNotNull(emp);
     }
 
-//    @Test
-//    public void setAndGetName() {
-//        emp.setFirstName(FIRST_NAME);
-//        String name = emp.getFirstName();
-//        Assert.assertNotNull(name);
-//        Assert.assertEquals(FIRST_NAME, name);
-//    }
-//    //
-//    @Test
-//    public void setAndGetLastName() {
-//        emp.setLastName(LAST_NAME);
-//        String name = emp.getLastName();
-//        Assert.assertNotNull(name);
-//        Assert.assertEquals(LAST_NAME, name);
-//    }
-//
-//    @Test
-//    public void setAndGetMiddleInitial() {
-//        emp.setMiddleInitial(MIDDLE_INITIAL);
-//        Character middleInitial = emp.getMiddleInitial();
-//        Assert.assertNotNull(middleInitial);
-//        Assert.assertEquals(MIDDLE_INITIAL, middleInitial);
-//    }
-//
-//    @Test
-//    public void setAndGetManager() {
-//        emp.setManager(mgr);
-//        Employee manager = emp.getManager();
-//        Assert.assertNotNull(manager);
-//        Assert.assertEquals(mgr.getId(), manager.getId());
-//    }
-//
-//    @Test
-//    public void setAndGetDepartment(){
-//        emp.setDepartment(dept);
-//        Assert.assertNotNull(emp.getDepartment());
-//        Assert.assertEquals(dept.getId(),emp.getDepartment().getId());
-//    }
-//
-//    @Test
-//    public void setAndGetJobTitle(){
-//        emp.setJobTitle(title);
-//        Assert.assertNotNull(emp.getJobTitle());
-//        Assert.assertEquals(title.getId(), emp.getJobTitle().getId());
-//    }
-//
-//    @Test
-//    public void setAndGetEmail(){
-//        emp.setEmail(EMAIL);
-//        Assert.assertNotNull(emp.getEmail());
-//        Assert.assertEquals(emp.getEmail(), EMAIL);
-//    }
-//
-//    @Test
-//    public void setAndGetSkypeName(){
-//        emp.setSkypeName(SKYPE_NAME);
-//        Assert.assertNotNull(emp.getSkypeName());
-//        Assert.assertEquals(emp.getSkypeName(), SKYPE_NAME);
-//    }
-//
-//    @Test
-//    public void setAndGetIsManager(){
-//        emp.setIsManager(true);
-//        Assert.assertNotNull(emp.getIsManager());
-//        Assert.assertTrue(emp.getIsManager());
-//    }
+    @Test
+    public void setAndGetName() {
+        emp.setFirstName(FIRST_NAME);
+        String name = emp.getFirstName();
+        Assert.assertNotNull(name);
+        Assert.assertEquals(FIRST_NAME, name);
+    }
+
+    @Test
+    public void setAndGetLastName() {
+        emp.setLastName(LAST_NAME);
+        String name = emp.getLastName();
+        Assert.assertNotNull(name);
+        Assert.assertEquals(LAST_NAME, name);
+    }
+
+    @Test
+    public void setAndGetMiddleInitial() {
+        emp.setMiddleInitial(MIDDLE_INITIAL);
+        Character middleInitial = emp.getMiddleInitial();
+        Assert.assertNotNull(middleInitial);
+        Assert.assertEquals(MIDDLE_INITIAL, middleInitial);
+    }
+
+    @Test
+    public void setAndGetManager() {
+        emp.setManager(mgr);
+        Employee manager = emp.getManager();
+        Assert.assertNotNull(manager);
+        Assert.assertEquals(mgr.getId(), manager.getId());
+    }
+
+    @Test
+    public void setAndGetDepartment(){
+        emp.setDepartment(dept);
+        Assert.assertNotNull(emp.getDepartment());
+        Assert.assertEquals(dept.getId(), emp.getDepartment().getId());
+    }
+
+    @Test
+    public void setAndGetJobTitle(){
+        emp.setJobTitle(title);
+        Assert.assertNotNull(emp.getJobTitle());
+        Assert.assertEquals(title.getId(), emp.getJobTitle().getId());
+    }
+
+    @Test
+    public void setAndGetEmail(){
+        emp.setEmail(EMAIL);
+        Assert.assertNotNull(emp.getEmail());
+        Assert.assertEquals(emp.getEmail(), EMAIL);
+    }
+
+    @Test
+    public void setAndGetSkypeName(){
+        emp.setSkypeName(SKYPE_NAME);
+        Assert.assertNotNull(emp.getSkypeName());
+        Assert.assertEquals(emp.getSkypeName(), SKYPE_NAME);
+    }
+
+    @Test
+    public void setAndGetIsManager(){
+        emp.setIsManager(true);
+        Assert.assertNotNull(emp.getIsManager());
+        Assert.assertTrue(emp.getIsManager());
+    }
 
 }
